6c1f4044626e9b5ce350a79b24af1e2c85439d91
Willie Shen <williesh@usc.edu>
2017-05-15 17:39:10 -0700

    asdf

--- prog2.py
+++ prog2.py
@@ -2,16 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(7, GPIO.OUT)
 
-while True:
-    input_state = GPIO.input(12)
+def Blink(speed):
+    GPIO.output(7,True)
+    time.sleep(speed)
+    GPIO.output(7,False)
+    time.sleep(speed)
+    GPIO.cleanup()
 
-if input_state == False:
-    print('Button Pressed')
-GPIO.output(7, True)
-else :
-    print('Button Not Pressed')
-GPIO.output(7, False)
+Blink(1)
